6fc60231a3d99b652e09fa132990c2b279f8f880
huycanqnnd <huy.tv@vietba.com.vn>
2020-12-24 11:06:06 +0700

    bài tập

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import React from 'react';
 import './App.css';
-
+import imp1 from "./images/1.jpg";
+import imp2 from "./images/2.jpg";
+import imp3 from "./images/3.jpg";
+import imp4 from "./images/4.jpg";
+import imp5 from "./images/5.jpg";
+import imp6 from "./images/6.jpg";
+import imp7 from "./images/7.jpg";
+import imp8 from "./images/8.jpg";
+import imp9 from "./images/9.jpg";
 function App() {
   let students = [
     { name: 'Binh', gender: 'Male', age: 28 },
@@ -44,51 +53,51 @@ function App() {
       <tr width = "100%">
           <td padding = "15px" width = "30%">
             <div align = "center">
-              <img class="image" src="images//1.jpg"/>
+              <img class="image" src={imp1}/>
             </div>
           </td>
           <td padding = "15px">
             <div align = "center">
-              <img class="image" src="images//2.jpg"/>
+              <img class="image" src={imp2}/>
             </div>
           </td>
           <td padding = "15px">
             <div align = "center">
-              <img class="image" src="images//3.jpg"/>
-            </div>
-          </td>
-        </tr>
-        <tr>
-          <td padding = "15px">
-            <div align = "center">
-              <img class="image" src="images//4.jpg"/>
-            </div>
-          </td>
-          <td padding = "15px">
-            <div align = "center">
-              <img class="image" src="images//5.jpg"/>
-            </div>
-          </td>
-          <td padding = "15px">
-            <div align = "center">
-              <img class="image" src="images//6.jpg"/>
+              <img class="image" src={imp3}/>
             </div>
           </td>
         </tr>
         <tr>
           <td padding = "15px">
             <div align = "center">
-              <img class="image" src="images//7.jpg"/>
+              <img class="image" src={imp4}/>
+            </div>
+          </td>
+          <td padding = "15px">
+            <div align = "center">
+              <img class="image" src={imp5}/>
+            </div>
+          </td>
+          <td padding = "15px">
+            <div align = "center">
+              <img class="image" src={imp6}/>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td padding = "15px">
+            <div align = "center">
+              <img class="image" src={imp7}/>
             </div>
           </td>
           <td padding = "15px">
             <div align = "center">      
-              <img class="image" src="images//8.jpg"/>
+              <img class="image" src={imp8}/>
             </div>
           </td>
           <td padding = "15px">
             <div align = "center">
-              <img class="image" src="images//9.jpg"/>
+              <img class="image" src={imp9}/>
             </div>
           </td>
         </tr>
